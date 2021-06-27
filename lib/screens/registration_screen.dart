d72a4a435699607545ff09b34a4f307c02db3cc7
hb57786 <tadeufcad@gmail.com>
2021-06-27 06:27:03 -0300

    Registering Users with Firebase using FirebaseAuth

--- lib/screens/registration_screen.dart
+++ lib/screens/registration_screen.dart
@@ -1,6 +1,8 @@
+import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
 import 'package:flash_chat/components/rounded_button.dart';
 import 'package:flash_chat/constants.dart';
+import 'chat_screen.dart';
 
 class RegistrationScreen extends StatefulWidget {
   static String id = 'registration_screen';
@@ -10,6 +12,7 @@ class RegistrationScreen extends StatefulWidget {
 }
 
 class _RegistrationScreenState extends State<RegistrationScreen> {
+  final _auth = FirebaseAuth.instance;
   String email;
   String password;
 
@@ -62,9 +65,16 @@ class _RegistrationScreenState extends State<RegistrationScreen> {
             RoundedButton(
               title: 'Register',
               colour: Colors.blueAccent,
-              onPressed: () {
-                print(email);
-                print(password);
+              onPressed: () async {
+                try {
+                  final newuser = await _auth.createUserWithEmailAndPassword(
+                      email: email.trim(), password: password.trim());
+                  if (newuser != null) {
+                    Navigator.pushNamed(context, ChatScreen.id);
+                  }
+                } catch (e) {
+                  print(e);
+                }
               },
             )
           ],
